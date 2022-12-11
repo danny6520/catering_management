ae137e3152dedd566d8459ba71ae167896730b6c
danny6520 <dannychristofer2001@gmail.com>
2022-12-11 17:03:30 +0530

    firstcommit

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Catering Management</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <link rel="stylesheet" href="global_style.css">
+</head>
+<body style="background-image: url(#); background-size: cover; background-repeat: no-repeat;">
+
+
+<nav class="navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Catering Management</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Catalog<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/c1.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="images/c2.jpg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="images/c3.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  
+  <center><h4>Catering Management</h4>
+  <h1>We Manage Your Events</h1>
+</center>
+  <hr>
+<div class="container">
+<center><h5>Allow our Chef to deliver the perfect private dinner or cocktail party experience in your home; or let us simply add a little extra flavor to your next office meeting, boat trip, or beach picnic. By partnering with our sibling location Rosewater Wine & Spirits, we can offer a full array of bar services and beverage selections. Email us to learn more.</h5></center>
+</div>
+
+
+<div class="container">
+  <div class="row">
+  <div class="col-sm-3">
+  <div id="example3" style="background-image: url('images/design4.jpg');">
+  <img src="images/img1.jpg" style="width:100%;">
+  </div>
+  </div>
+  <div class="col-sm-3">
+  <div id="example3" style="background-image: url('images/design4.jpg');">
+  <img src="images/img2.png" style="width:100%;">
+  </div>
+  </div>
+  <div class="col-sm-3">
+  <div id="example3" style="background-image: url('images/design4.jpg');">
+  <img src="images/img3.png" style="width:100%;">
+  </div>
+  </div>
+  <div class="col-sm-3">
+  <div id="example3" style="background-image: url('images/design4.jpg');">
+  <img src="images/img4.png" style="width:100%;">
+  </div>
+  </div>
+</div>
+</div>
+<br>
+
+<center><h1>Contact us now!</h1>
+</center><hr>
+
+<div class="container">
+  <div class="row" style="background-image: url('images/design5.jpeg');">
+  <div class="col-sm-6">
+  <img src="images/contact2.png" style="width:100%;">
+</div>
+<div class="col-sm-6">
+  <div id="example1" style="background-image: url('images/design2.jpg');">
+<form>
+<label>Your Name: </label><br>
+<input type="text" class="form-control" placeholder="Your Name" required/><br>
+<label>Mobile: </label><br>
+<input type="text" class="form-control" placeholder="Your Mobile" required/><br>
+<label>Email: </label><br>
+<input type="text" class="form-control" placeholder="Your Email" required/><br>
+<label>Your rating for our service (bad/good/excellent): </label>
+  <input type="range" id="vol" name="vol" min="0" max="2"><br>
+<label>Queries: </label><br>
+<textarea type="text" class="form-control" placeholder="Type your queries here..." required></textarea><br>
+<center><button type="submit" class="btn btn-success">Submit</button></center>
+</form>
+<br>
+<div id="example2">
+<center><h4>Reach out to us..</h4></center><hr>
+<p>Email: <b>contactus@catering.com</b></p>
+<p>Mobile: <b>+91 1231231231</b></p>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<br>
+
+<!-- FOOTER START -->
+<div class="footer">
+  <div class="contain">
+  <div class="col">
+    <h1>Company</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Products</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Accounts</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Resources</h1>
+    <ul>
+      <li>Webmail</li>
+      <li>Redeem code</li>
+      <li>WHOIS lookup</li>
+      <li>Site map</li>
+      <li>Web templates</li>
+      <li>Email templates</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Support</h1>
+    <ul>
+      <li>Contact us</li>
+      <li>Web chat</li>
+      <li>Open ticket</li>
+    </ul>
+  </div>
+  <div class="col social">
+    <h1>Social</h1>
+    <ul>
+      <li><img src="https://svgshare.com/i/5fq.svg" width="32" style="width: 32px;"></li>
+      <li><img src="https://svgshare.com/i/5eA.svg" width="32" style="width: 32px;"></li>
+      <li><img src="https://svgshare.com/i/5f_.svg" width="32" style="width: 32px;"></li>
+    </ul>
+  </div>
+<div class="clearfix"></div>
+
+</div>
+</div>
+
+<div class="footerin">
+<center><font color="black"><b><p>CATERING MANAGEMENT</p></b></font></center>
+</div>
+<!-- END OF FOOTER -->
+
+  
+</body>
+</body>
+</html>
